7762bec1f78e896b21506b888cdcd92941da91ee
Valerie <valerieo@udel.edu>
2026-03-04 15:03:51 -0500

    ChangeType and StartAttempt completely implemented and functional

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question",
+    );
+
+    function flipQuestion(): void {
+        setQuestion(
+            question === "short_answer_question" ?
+                "multiple_choice_question"
+            :   "short_answer_question",
+        );
+    }
+
+    return (
+        <div>
+            <Button onClick={flipQuestion}>Change Type</Button>
+            <div>
+                {question === "short_answer_question" ?
+                    <span>Short Answer</span>
+                :   <span>Multiple Choice</span>}
+            </div>
+        </div>
+    );
 }
