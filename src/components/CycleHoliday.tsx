87d25f4fe92bcff871abc7666d5fe34da0632548
Valerie <valerieo@udel.edu>
2026-03-04 15:58:05 -0500

    CycleHoliday completed, all tests passed

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,35 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holidays, setHoliday] = useState<string>("🐲");
+
+    function sortByAlphabet(): void {
+        setHoliday(
+            holidays === "🐲" ? "🎁"
+            : holidays === "🎁" ? "🌎"
+            : holidays === "🌎" ? "🍀"
+            : holidays === "🍀" ? "💖"
+            : "🐲",
+        );
+    }
+
+    function sortByDate(): void {
+        setHoliday(
+            holidays === "💖" ? "🐲"
+            : holidays === "🐲" ? "🍀"
+            : holidays === "🍀" ? "🌎"
+            : holidays === "🌎" ? "🎁"
+            : "💖",
+        );
+    }
+
+    return (
+        <div>
+            Holiday: {holidays}
+            <div>
+                <Button onClick={sortByAlphabet}>Advance By Alphabet</Button>
+                <Button onClick={sortByDate}>Advance By Year</Button>
+            </div>
+        </div>
+    );
 }
