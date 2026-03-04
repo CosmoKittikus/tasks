2082f39796bd88117dca39f485b3ef5d0857497a
Valerie <valerieo@udel.edu>
2026-03-04 15:20:21 -0500

    TwoDice completed

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,29 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [dice1, setDice1] = useState<number>(1);
+    const [dice2, setDice2] = useState<number>(2);
+
+    function rollRight(): void {
+        setDice2(d6());
+    }
+
+    function rollLeft(): void {
+        setDice1(d6());
+    }
+
+    return (
+        <div>
+            <span data-testid="left-die">{dice1}</span>
+            <span data-testid="right-die">{dice2}</span>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            <div>
+                {dice1 === dice2 && dice1 !== 1 && <span>Win</span>}
+                {dice1 === dice2 && dice1 === 1 && <span>Lose</span>}
+            </div>
+        </div>
+    );
 }
